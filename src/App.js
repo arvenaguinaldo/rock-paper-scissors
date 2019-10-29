faf1ecef4a0ec011a4c4adb61db7ff1f09b0104c
Arven Aguinaldo <arvencaguinaldo@gmail.com>
2019-10-29 20:27:20 +0800

    change html title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Player from './containers/Player/Player'
 import Home from './containers/Home/Home'
@@ -7,7 +7,7 @@ import Computer from './containers/Computer/Computer'
 
 function App() {
   return (
-    <Router>
+    <Router basename="rock-paper-scissors">
       <div className="App">
         <Switch>
           <Route path="/" exact component={Home} />
