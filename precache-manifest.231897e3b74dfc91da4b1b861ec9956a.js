8c7a659520c8915d7db017ce53ac69c9153da423
Arven Aguinaldo <arvencaguinaldo@gmail.com>
2019-10-29 20:03:09 +0800

    Updates

---
+++ precache-manifest.231897e3b74dfc91da4b1b861ec9956a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad322a156b2fefbdda54e8e90dddb2d",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "51f5a9dcda53972b9a39",
+    "url": "/rock-paper-scissors/static/css/main.0018c944.chunk.css"
+  },
+  {
+    "revision": "8b658b3fc5d89dea453f",
+    "url": "/rock-paper-scissors/static/js/2.7a19f81d.chunk.js"
+  },
+  {
+    "revision": "51f5a9dcda53972b9a39",
+    "url": "/rock-paper-scissors/static/js/main.4836e53e.chunk.js"
+  },
+  {
+    "revision": "affb9b2bbdc2668b1ba3",
+    "url": "/rock-paper-scissors/static/js/runtime-main.168b54f6.js"
+  },
+  {
+    "revision": "431d47b028a311645b90d2f346779f86",
+    "url": "/rock-paper-scissors/static/media/paper.431d47b0.png"
+  },
+  {
+    "revision": "67841ae735b2592f7c15aaa0b10df5f7",
+    "url": "/rock-paper-scissors/static/media/rock.67841ae7.png"
+  },
+  {
+    "revision": "72e4797bf2059cd1d5c6342836034750",
+    "url": "/rock-paper-scissors/static/media/scissors.72e4797b.png"
+  }
+]);
